3dc2e4f2b5ea91f855bcff54d42b13ed936328e0
ynagata.org <strangamo1986@gmail.com>
2015-12-20 13:31:47 +0900

    C# コンソールアプリケーションで "Hello, World!" してみました。

---
+++ CSharpConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! @ {0}", DateTime.Now);
+            Console.WriteLine("5! == {0}", Program.Factorial(5));
+
+            // 次の行はコンパイル前に VS が叱ってくれる。
+            // var x = Program.Factorial(-1);
+        }
+
+        private static uint Factorial(uint n)
+        {
+            return n == 0 ? 1 : n * Program.Factorial(n - 1);
+        }
+    }
+}
