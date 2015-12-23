fb80708defc554763385300d61bb0933a8ad61ee
ynagata.org <strangamo1986@gmail.com>
2015-12-23 09:12:26 +0900

    再帰ではなくコレクションをこねる感じで、階乗を書いてみます。

--- CSharpConsole/Program.cs
+++ CSharpConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace CSharpConsole
 {
@@ -8,14 +9,19 @@ namespace CSharpConsole
         {
             Console.WriteLine("Hello, World! @ {0}", DateTime.Now);
             Console.WriteLine("5! == {0}", Program.Factorial(5));
-
-            // 次の行はコンパイル前に VS が叱ってくれる。
-            // var x = Program.Factorial(-1);
+            Console.WriteLine("10! == {0}", Program.Factorial2(10));
         }
 
         private static uint Factorial(uint n)
         {
             return n == 0 ? 1 : n * Program.Factorial(n - 1);
+        }
+
+        private static uint Factorial2(uint n)
+        {
+            return n > int.MaxValue 
+                ? uint.MaxValue
+                : (uint)Enumerable.Range(1, (int)n).Aggregate((a, b) => a * b);
         }
     }
 }
--- PythonConsole/PythonConsole.py
+++ PythonConsole/PythonConsole.py
@@ -11,6 +11,19 @@ def fact(n):
     else:
         return n * fact(n - 1)
 
+def fact2(n):
+    if not(isinstance(n, int)):
+        raise ValueError("fact には引数として 0 以上の整数を与えてください。")
+    if (n < 0):
+        raise ValueError("fact には引数として 0 以上の整数を与えてください。")
+    else:
+        ret = 1
+        for var in xrange(1, n + 1):
+            ret = ret * var
+        return ret;
+
+
 if __name__ == '__main__':
     print 'Hello, World! @ {0}'.format(datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
     print '5! = {0}'.format(fact(5))
+    print '10! = {0}'.format(fact2(10))
