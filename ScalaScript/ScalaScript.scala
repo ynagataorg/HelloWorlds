356afdb463eadd71c11db23fc5755bec2f818bcf
ynagataorg <strangamo1986@gmail.com>
2016-01-03 10:15:02 +0900

    せっかくなので @tailrec にしておきます。

--- ScalaScript/ScalaScript.scala
+++ ScalaScript/ScalaScript.scala
@@ -1,7 +1,13 @@
+import scala.annotation.tailrec
 import java.util.Date
 var datestr = "%tY/%<tm/%<td %<tT" format new Date
 
-def fact(n: Int): Int = if (n <= 1) 1 else n * fact(n - 1)
+def fact(n: Int): Int = {
+  @tailrec
+  def factIter(prod: Int, n: Int): Int =
+    if (n == 1) prod else factIter(prod * n, n - 1)
+  if (n <= 1) 1 else factIter(1, n)
+}
 
 println("Hello, world! @ " + datestr)
 println("fact(5) == " + fact(5))
